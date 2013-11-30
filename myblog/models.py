506ee4446fdc27a6023b154f30d394b28a873ff7
johnnywsd <shoudaw@gmail.com>
2013-11-29 19:57:59 -0500

    Initial commit

---
+++ myblog/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Blog(models.Model):
+    id = models.AutoField(primary_key=True)
+    authors = models.ManyToManyField(User)
+
+class Tag(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(User)
+    blog = models.ForeignKey(Blog)
+    title = models.CharField(max_length=400)
+    content = models.TextField(blank=True, null=True)
+    #comments = models.OneToManyField(Comment)
+    tags = models.ManyToManyField(Tag)
+    create_time = models.DateTimeField(auto_now_add=True)
+    modify_time = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    post = models.ForeignKey(Post)
+    author = models.ForeignKey(User)
+    content = models.TextField(blank=True, null=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+
+# Create your models here.
